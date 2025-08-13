441d8f870e4403d2000a85ee8a6ba1f74bb9e665
asepsbr <asepsubari007@gmail.com>
2025-08-13 15:49:13 +0700

    Remove frontend folder from main repo tracking

--- frontend/src/pages/LogsReports/index.jsx
+++
@@ -1,3 +0,0 @@
-export default function LogsReports() {
-  return <div className="text-gray-700">Logs and Reports - Lorem ipsum dolor sit amet.</div>;
-}
--- frontend/src/pages/PipelineManagement/index.jsx
+++
@@ -1,3 +0,0 @@
-export default function PipelineManagement() {
-  return <div className="text-gray-700">Pipeline Management - Lorem ipsum dolor sit amet.</div>;
-}
